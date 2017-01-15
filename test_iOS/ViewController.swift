d59bcc6bbbcdf3bd62945d91f583a790b0e8f7a4
Brandon Koch <brandonkoch@KOCH-MACBOOKPRO.local>
2017-01-14 22:24:27 -0700

    Initial Commit

---
+++ test_iOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  test_iOS
+//
+//  Created by Brandon Koch on 1/14/17.
+//  Copyright © 2017 Brandon Koch. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
